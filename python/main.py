b584941dc3664b3a829047022b7aa9620f30b88a
Diego Garza <diego.garzatr@uanl.edu.mx>
2017-05-12 02:42:27 -0500

    after clean

--- python/main.py
+++ python/main.py
@@ -2,22 +2,13 @@ import kociemba
 import config
 import conns
 import solution
-#Todo List
-#Configurar puertos en Arduino
-#comprobar conexion
-#set null terminator to solution string
-#Solución temporal:
-#  Agregar una x al final de la string para que el arduino sepa cuando se termina
-#
+
 def main():
     conns.startConnection()
-    if(conns.isSerialConnected() is True):
+    if(conns.isSerialConnected() == True):
         solution.sendSolution()
     else:
         print("Error")
-    #print("Por favor, introduce el estado actual del cubo")
-
-
 
 if __name__ == '__main__':
     main()
