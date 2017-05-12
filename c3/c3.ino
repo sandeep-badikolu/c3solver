a4552cf2e814fd32b8e6b625d83396b1835dea21
Diego Garza <diego.garzatr@uanl.edu.mx>
2017-05-12 09:23:37 -0500

    added flags for the movements

--- c3/c3.ino
+++ c3/c3.ino
@@ -1,6 +1,5 @@
 // Definición de los pines para controlar los motores.
 // Conexiones para el arduino nano/uno
-
 const int stepPinR = 13;
 const int stepPinL = 11;
 const int stepPinU = 9;
@@ -14,22 +13,16 @@ const int dirPinU = 8;
 const int dirPinD = 6;
 const int dirPinF = 4;
 const int dirPinB = 2;
-
 // Variable para guardar los pasos recibidos
-String solution;
+String solution = "";
+
+// Flag para que inicie los movimientos 
+boolean start = false;
 
 void setup() {
   //Iniciamos el /baud rate/ de lacomunicación serial para la transmisión de datos
   Serial.begin(9600);
 
-//  for(int i = 0; i < 10; i++){
-//    Serial.println("1");
-//  }
-
-  while(Serial.available() == false){
-    Serial.println("no se hizo la conexión serial");
-    delay(1000);
-  }
   //Definimos los pins de cada motor como salidas
   pinMode(stepPinR, OUTPUT);
   pinMode(dirPinR, OUTPUT);
@@ -43,23 +36,33 @@ void setup() {
   pinMode(dirPinF, OUTPUT);
   pinMode(stepPinB, OUTPUT);
   pinMode(dirPinB, OUTPUT);
-  // La solución se irá recibiendo de paso a paso la solución, iniciamos la string
-  solution = "";
 }
 
 void loop() {
-  while(Serial.available()) { // Mientras se reciba algo, añade el paso entrante a la string de solución 
+  if(Serial.available() == false){
+    Serial.println("no se hizo la conexión serial");
+    Serial.println("1");
+    delay(1000);
+  }
+  
+  else if(Serial.available() > 0) { // Mientras se reciba algo, añade el paso entrante a la string de solución 
     char pasoEntrante = (char)Serial.read();
     solution += pasoEntrante;
     Serial.println(pasoEntrante);
     }
+  
+
+  else{
+    Serial.println("standby");
   }
-  int i;
-  for(i = 0; i < solution.length(); i++){
+  
+  if(start == true){
+  for(int i = 0; i < solution.length(); i++){
     switch(solution.charAt(i)){
       case 'R':
       // Definimos la dirección en la que se moverá el motor, en este caso en sentido horario.
         digitalWrite(dirPinR, HIGH);
+        
         // Nuestro motor tiene un ángulo por paso de 1.8 grados, por lo que si queremos que gire 90 grados dividimos 90/1.8 lo que nos da el total de pasos que es 50.
         for(int i = 0; i < 50; i++){
           digitalWrite(stepPinR, HIGH);
@@ -193,6 +196,7 @@ void loop() {
         Serial.println("No se reconoció el movimiento");
         delay(5000);
         break;
+    }
     }
   }
   
